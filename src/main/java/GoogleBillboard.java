93c1ffe0ee0fbe7d00143a38784acb4d99dea9f9
T7r1 <142773849+T7r1@users.noreply.github.com>
2024-01-17 12:55:17 -0800

    Update GoogleBillboard.java

--- src/main/java/GoogleBillboard.java
+++ src/main/java/GoogleBillboard.java
@@ -1,8 +1,6 @@
-public final static String e = "2.7182818284590452353602874713526624977572470936999595749669676277240766303535475945713821785251664274274663919320030599218174135966290435";
-double ans=0;
-
-public void setup()
-{
+public class GoogleBillboard {
+	public final static String e = "2.7182818284590452353602874713526624977572470936999595749669676277240766303535475945713821785251664274274663919320030599218174135966290435";  
+	public static void main(final String[] args) {
   String digits ="";
   double dNum = 0;
   //System.out.println(e.length()-2);  //displays 7.182818284E9}
@@ -17,7 +15,7 @@ public void setup()
 
 //ANSWER (uncomment it in processing for answer)
 
-     //System.out.println(ans);
+     System.out.println(ans);
 
 //AMSWER (uncomment it in processing for answer)
 
@@ -25,33 +23,11 @@ public void setup()
       return;
     }
   }
-public class GoogleBillboard {
-	public final static String e = "2.7182818284590452353602874713526624977572470936999595749669676277240766303535475945713821785251664274274663919320030599218174135966290435";  
-	public static void main(final String[] args) {
-		String digits ="";
-  double dNum = 0;
-  //System.out.println(e.length()-2);  //displays 7.182818284E9}
 
-  for (int i=2; i<(e.length()-10); i++) {
-    digits = e.substring(i, i+10);
-    dNum = Double.parseDouble(digits);
-    //System.out.println(dNum);  //displays 7.182818284E9}
-    if (isPrime(dNum)) {
-      ans=dNum;
-
-
-//ANSWER (uncomment it in processing for answer)
-System.out.println(ans);
-     //System.out.println(ans);
-
-//AMSWER (uncomment it in processing for answer)
-
-
-      return;
-		System.out.println("Replace this string with your answer!");
+		//System.out.println("Replace this string with your answer!");
 	}
-  }
-  }
+
+
 	//Finish this function
 	public boolean isPrime(double num) {
   if (num>=2) {
@@ -66,3 +42,14 @@ System.out.println(ans);
 
 }
 
+
+public final static String e = "2.7182818284590452353602874713526624977572470936999595749669676277240766303535475945713821785251664274274663919320030599218174135966290435";
+double ans=0;
+
+public void setup()
+
+
+public void draw()
+{
+  //not needed for this assignment
+}
