24121abf9a6ccb2d73264e56f094c337e93e1d5c
T7r1 <142773849+T7r1@users.noreply.github.com>
2024-01-17 12:47:25 -0800

    Update GoogleBillboard.java

--- src/main/java/GoogleBillboard.java
+++ src/main/java/GoogleBillboard.java
@@ -1,15 +1,68 @@
+public final static String e = "2.7182818284590452353602874713526624977572470936999595749669676277240766303535475945713821785251664274274663919320030599218174135966290435";
+double ans=0;
+
+public void setup()
+{
+  String digits ="";
+  double dNum = 0;
+  //System.out.println(e.length()-2);  //displays 7.182818284E9}
+
+  for (int i=2; i<(e.length()-10); i++) {
+    digits = e.substring(i, i+10);
+    dNum = Double.parseDouble(digits);
+    //System.out.println(dNum);  //displays 7.182818284E9}
+    if (isPrime(dNum)) {
+      ans=dNum;
 
 
+//ANSWER (uncomment it in processing for answer)
+
+     //System.out.println(ans);
+
+//AMSWER (uncomment it in processing for answer)
+
+
+      return;
+    }
+  }
 public class GoogleBillboard {
 	public final static String e = "2.7182818284590452353602874713526624977572470936999595749669676277240766303535475945713821785251664274274663919320030599218174135966290435";  
 	public static void main(final String[] args) {
+		String digits ="";
+  double dNum = 0;
+  //System.out.println(e.length()-2);  //displays 7.182818284E9}
+
+  for (int i=2; i<(e.length()-10); i++) {
+    digits = e.substring(i, i+10);
+    dNum = Double.parseDouble(digits);
+    //System.out.println(dNum);  //displays 7.182818284E9}
+    if (isPrime(dNum)) {
+      ans=dNum;
+
+
+//ANSWER (uncomment it in processing for answer)
+System.out.println(ans);
+     //System.out.println(ans);
+
+//AMSWER (uncomment it in processing for answer)
+
+
+      return;
 		System.out.println("Replace this string with your answer!");
 	}
-	
-
+  }
+  }
 	//Finish this function
-	public boolean isPrime(double d){
-	}
+	public boolean isPrime(double num) {
+  if (num>=2) {
+    for (int i = 2; i <= Math.sqrt(num); i++) {
+      if (num%i==0)
+        return false;
+    }
+    return true;
+  }
+  return false;
+}
 
 }
 
