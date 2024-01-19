513f24ab72d2e8dd5fbdc68c0bedf5fb9d639ef1
T7r1 <142773849+T7r1@users.noreply.github.com>
2024-01-19 09:33:38 -0800

    Update GoogleBillboard.java

--- src/main/java/GoogleBillboard.java
+++ src/main/java/GoogleBillboard.java
@@ -26,10 +26,11 @@ public class GoogleBillboard {
 
 		//System.out.println("Replace this string with your answer!");
 	}
+}
 
 
 	//Finish this function
-	public boolean isPrime(double num) {
+public boolean isPrime(double num) {
   if (num>=2) {
     for (int i = 2; i <= Math.sqrt(num); i++) {
       if (num%i==0)
@@ -39,17 +40,7 @@ public class GoogleBillboard {
   }
   return false;
 }
-
 }
 
 
-public final static String e = "2.7182818284590452353602874713526624977572470936999595749669676277240766303535475945713821785251664274274663919320030599218174135966290435";
-double ans=0;
 
-public void setup()
-
-
-public void draw()
-{
-  //not needed for this assignment
-}
